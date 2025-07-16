9293f970745c6d0a7d000cc8ca60ed0630c75ca4
kaamilbravo <kaamilbravoay@gmail.com>
2025-07-16 06:42:10 -0400

    finished all code except sending mail as company has firewall

--- MainApp/pages/Contact_Info.py
+++ MainApp/pages/Contact_Info.py
@@ -2,10 +2,12 @@ import streamlit as st
 
 st.header("Contact Info")
 
+choice = st.selectbox("Please selecet a type of complaint:",
+                     ('Angry','Upset','Distraught'))                      
+
 with st.form(key = "Mail_info"):
         email = st.text_input("Please enter your email Address")
         message = st.text_area("Please enter you query")
         button = st.form_submit_button("Submit")
 if button:
-    print(email,message)
-print("hello")
+    print(f"From {email}: {choice} \n{message}")
