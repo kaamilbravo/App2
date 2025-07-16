a64c6f0c2d03ce839a6df147555700de8ef469f5
kaamilbravo <kaamilbravoay@gmail.com>
2025-07-16 04:22:31 -0400

    added width, url and finish

--- MainProj/MyInfopage.py
+++ MainProj/MyInfopage.py
@@ -11,24 +11,28 @@ with col2:
     content = """Kaamil Rifaq is a final-year Mechanical Engineering student at NUST with strong leadership, technical, and industrial experience. He has led award-winning engineering projects, interned at Atlas Honda, and co-founded a sustainability-focused startup. Kaamil aims to drive innovation in renewable energy and engineering through global exposure and advanced education."""
     st.write(content)
 
-st.write("sdfsdfsf This is the First initialization of the app sdfsadfsafsfasdfsfasdf f asf asdfs fd fsd fasf f sdf afasfa sf sd sd")
+st.write("This is the first initialization")
 
-col3,col4 = st.columns(2)
+col3, gap, col4 = st.columns([3,1,3])
 
 df = read_csv("MainProj/data.csv",sep = ";")
 print(df)
+
 with col3:
     for index,classes in df.iterrows():
         if index % 2 == 0:
             st.title(classes["title"])
             st.text(classes["description"])
-            st.image(f"MainProj/pictures/{index+1}.png")
+            st.image(f"MainProj/pictures/{classes["image"]}")
+            st.write(f"[Source Code]({classes["url"]})")
+
 with col4:
-    for index,classes in df.iterrows():
+    for index,classes in df.iterrows():          # Very Important iterrows
         if index % 2 != 0:
             st.title(classes["title"])
             st.text(classes["description"])
-            st.image(f"MainProj/pictures/{index+1}.png")
+            st.image(f"MainProj/pictures/{classes["image"]}")
+            st.write(f"[Source Code]({classes["url"]})")
         
 
 #with col4
