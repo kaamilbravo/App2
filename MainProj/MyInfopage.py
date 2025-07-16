239ecd5d402192755b1cf5ce4f0bde2f8ce45c6c
kaamilbravo <kaamilbravoay@gmail.com>
2025-07-16 03:24:15 -0400

    add text

--- MainProj/MyInfopage.py
+++ MainProj/MyInfopage.py
@@ -10,3 +10,5 @@ with col2:
     st.title("Kaamil Rifaq")
     content = """Kaamil Rifaq is a final-year Mechanical Engineering student at NUST with strong leadership, technical, and industrial experience. He has led award-winning engineering projects, interned at Atlas Honda, and co-founded a sustainability-focused startup. Kaamil aims to drive innovation in renewable energy and engineering through global exposure and advanced education."""
     st.write(content)
+
+st.write("This is the First initialization of the app sdfsadfsafsfasdfsfasdf f asf asdfs fd fsd fasf f sdf afasfa sf sd sd")
